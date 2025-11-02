4971c91517f8dae6b00a2f847da089b82953b903
Gabby Simpson <gabby@dotunwrap.dev>
2025-11-02 18:11:37 -0500

    feat: Initial commit

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt-tree;
+
+          packages.default = pkgs.stdenv.mkDerivation {
+            pname = "dwm";
+            version = "6.4";
+            src = ./src-patched;
+
+            buildInputs = with pkgs.xorg; [
+              libX11
+              libXft
+              libXinerama
+              libxcb
+            ];
+
+            buildPhase = ''
+              make
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              mv dwm $out/bin
+            '';
+
+            meta = {
+              description = "Dynamic window manager for X - Gabby's Version";
+              license = pkgs.lib.licenses.mit;
+            };
+          };
+        };
+    };
+}
---
+++ justfile
@@ -0,0 +1,37 @@
+PATCHES_DIR := "./patches"
+SRC_DIR := "./src"
+OUTPUT_DIR := "./src-patched"
+
+patch:
+  #!/usr/bin/env bash
+  set -e
+
+  if [ ! -d "{{OUTPUT_DIR}}" ]; then
+    echo "Generating {{OUTPUT_DIR}}..."
+    mkdir -p {{OUTPUT_DIR}}
+    cp {{SRC_DIR}}/* {{OUTPUT_DIR}}/ && chmod -R +w {{OUTPUT_DIR}}
+    touch {{OUTPUT_DIR}}/patches-applied.txt
+  else
+    rm -f {{OUTPUT_DIR}}/*.rej
+    rm -f {{OUTPUT_DIR}}/*.orig
+  fi
+
+  while IFS= read -r patch_file; do
+    if grep -q "^$patch_file$" {{OUTPUT_DIR}}/patches-applied.txt; then
+      echo "Warning: {{PATCHES_DIR}}/$patch_file already applied, skipping."
+      continue
+    fi
+
+    if [ -f "patches/$patch_file" ]; then
+      echo "Applying $patch_file..."
+      echo $patch_file >> {{OUTPUT_DIR}}/patches-applied.txt
+      patch -p1 -d {{OUTPUT_DIR}}/ < "{{PATCHES_DIR}}/$patch_file"
+    else
+      echo "Warning: {{PATCHES_DIR}}/$patch_file not found, skipping."
+    fi
+  done < {{PATCHES_DIR}}/patch-order.txt
+
+  rm -f {{OUTPUT_DIR}}/*.rej
+  rm -f {{OUTPUT_DIR}}/*.orig
+
+  cp {{OUTPUT_DIR}}/config.def.h {{OUTPUT_DIR}}/config.def.h.orig
